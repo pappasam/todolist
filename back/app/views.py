d994ac4cfd84c98d26a929289b860f1d44d3bc64
Sam Roeca <samuel.roeca@gmail.com>
2016-05-14 13:51:27 -0400

    Add PATCH method to notes, enabling updating of content

--- back/app/views.py
+++ back/app/views.py
@@ -16,14 +16,18 @@ class ApiNotes(Resource):
     schema = NotesSchema()
 
     def get(self):
+        '''Retrive all notes and all content
+
+        Return json with list of notes
+        '''
         db_query_results = Notes.query.all()
         notes = self.schema.dump(db_query_results, many=True).data
         return jsonify({"notes": notes})
 
     def post(self):
-        '''Add new entry to database
+        '''Add new note to database
 
-        Return json value for the note
+        Return json value for the note's value
         '''
         js_data = request.get_json(force=True)
         notes_entry = Notes(**js_data)
@@ -31,6 +35,22 @@ class ApiNotes(Resource):
         db.session.commit()
         note_json = self.schema.dump(notes_entry).data
         return jsonify({"note": note_json})
+
+    def patch(self):
+        '''Update the content of a note
+
+        Return json value for old note's value
+        '''
+        js_data = request.get_json(force=True)
+        note = Notes.query.filter_by(id=js_data['id']).first()
+        note_content_original = self.schema.dump(note).data
+        note.content = js_data['content']
+        note_content_new = self.schema.dump(note).data
+        db.session.commit()
+        return jsonify({
+            'note_original': note_content_original,
+            'note_new': note_content_new,
+        })
 
 resources = OrderedDict()
 resources['/notes'] = ApiNotes
